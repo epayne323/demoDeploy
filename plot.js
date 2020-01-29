3befca1b9cd8923a6d5d7983a603d004c6dafe43
Eli Payne <epayne323@gmail.com>
2020-01-28 20:51:01 -0600

    stuff

--- plot.js
+++ plot.js
@@ -1,46 +1,42 @@
-console.log(data);
+// Use d3.json() to fetch data from JSON file
+// Incoming data is internally referred to as incomingData
+d3.json("data/data.json").then((incomingData) => {
+  function filterMovieRatings(movie) {
+    return movie.imdbRating > 8.9;
+  }
 
-// Create Trace1 for the Greek Data
+  // Use filter() to pass the function as its argument
+  var filteredMovies = incomingData.filter(filterMovieRatings);
 
-var pairs = data.map(god => god.pair);
-var greekResults = data.map(god => god.greekSearchResults);
-var romanResults = data.map(god => god.romanSearchResults);
-    
-var trace1 = 
-{
-    x: pairs,
-    y: greekResults,
-    type: "bar",
-    name: "Greek"
-};
+  //  Check to make sure your are filtering your movies.
+  console.log(filteredMovies);
 
+  // Use the map method with the arrow function to return all the filtered movie titles.
+  var titles = filteredMovies.map(movies =>  movies.title);
 
+  // Use the map method with the arrow function to return all the filtered movie metascores.
+  var ratings = filteredMovies.map(movies => movies.metascore);
 
-// Create Trace 2 for the Roman Data
-var trace2 = 
-{
-    x: pairs,
-    y: romanResults,
-    type: "bar",
-    name: "Roman"
-};
+  // Check your filtered metascores.
+  console.log(ratings);
 
+  // Create your trace.
+  var trace = {
+    x: titles,
+    y: ratings,
+    type: "bar"
+  };
 
+  // Create the data array for our plot
+  var data = [trace];
 
-// Combine both traces into an array
-var plot_data = [trace1,trace2];
+  // Define the plot layout
+  var layout = {
+    title: "The highest critically acclaimed movies.",
+    xaxis: { title: "Title" },
+    yaxis: { title: "Metascore (Critic) Rating"}
+  };
 
-
-
-// Create layout
-var layout = 
-{
-    title: "Greek vs. Roman gods search results",
-    yaxis: { title: "Search Results"},
-    xaxis: {automargin: true}
-};
-
-
-
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", plot_data, layout);
+  // Plot the chart to a div tag with id "bar-plot"
+  Plotly.newPlot("bar-plot", data, layout);
+});
